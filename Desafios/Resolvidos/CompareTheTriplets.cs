bfc09e0854b3ee5f72fbca7e9a65767390f8d192
Breno Narde Silva <brenonarde@gmail.com>
2023-11-21 08:03:39 -0300

    add compareTriplets

---
+++ Desafios/Resolvidos/CompareTheTriplets.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafios.Desafios.Resolvidos
+{
+    using System.CodeDom.Compiler;
+    using System.Collections.Generic;
+    using System.Collections;
+    using System.ComponentModel;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Runtime.Serialization;
+    using System.Text.RegularExpressions;
+    using System.Text;
+    using System;
+
+    class Result
+    {
+        public static List<int> compareTriplets(List<int> a, List<int> b)
+        {
+            int alice = 0;
+            int bob = 0;
+            int[] result = new int[2];
+            if (a.Count == b.Count)
+            {
+                for (int i = 0; i < a.Count; i++)
+                {
+                    if (a[i] > b[i])
+                    {
+                        alice++;
+                    }
+                    else if (a[i] < b[i])
+                    {
+                        bob++;
+                    }
+                }
+            }
+            result[0] = alice;
+            result[1] = bob;
+            return result.ToList();
+        }
+    }
+
+    class CompareTheTriplets
+    {
+        public static void Init(string[] args)
+        {
+            //@System.Environment.GetEnvironmentVariable("OUTPUT_PATH")
+            TextWriter textWriter = new StreamWriter(Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+            List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+            Result.compareTriplets(a, b);
+            List<int> result = Result.compareTriplets(a, b);
+
+            textWriter.WriteLine(string.Join(" ", result));
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Desafios.Desafios.Resolvidos;
+
+namespace Desafios
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CompareTheTriplets.Init(args);
+
+        }
+    }
+}
