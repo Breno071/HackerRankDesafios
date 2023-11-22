30861308d23f8609869ffdeed090b993699f4fa8
Breno Narde Silva <brenonarde@gmail.com>
2023-11-22 08:01:23 -0300

    add plusMinus

---
+++ Desafios/NaoResolvidos/PlusMinus.cs
@@ -0,0 +1,41 @@
+﻿using Desafios.Desafios.Resolvidos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafios.Desafios.NaoResolvidos
+{
+    public class PlusMinus
+    {
+        public static void Init()
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            plusMinus(arr);
+        }
+
+        internal static void plusMinus(List<int> arr)
+        {
+            double positivos = 0;
+            double negativos = 0;
+            double zeros = 0;
+            foreach (var item in arr)
+            {
+                if(item > 0) positivos += 1;
+                if (item < 0) negativos += 1;
+                if (item == 0) zeros += 1;
+            }
+
+            double positivosPorcentagem = positivos / arr.Count;
+            double negativosPorcentagem = negativos / arr.Count;
+            double zerosPorcentagem = zeros / arr.Count;
+            Console.WriteLine(positivosPorcentagem.ToString("F6"));
+            Console.WriteLine(negativosPorcentagem.ToString("F6"));
+            Console.WriteLine(zerosPorcentagem.ToString("F6"));
+        }
+    }
+}
