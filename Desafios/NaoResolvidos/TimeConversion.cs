4649f6eced214f07ae878bae9472c4e5c3ee5754
Breno Narde Silva <brenonarde@gmail.com>
2023-11-27 17:29:09 -0300

    add TimeConversion

---
+++ Desafios/NaoResolvidos/TimeConversion.cs
@@ -0,0 +1,38 @@
+﻿using Desafios.Desafios.Resolvidos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafios.Desafios.NaoResolvidos
+{
+    public class TimeConversion
+    {
+        internal static void Init()
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string s = Console.ReadLine();
+
+            string result = timeConversion(s);
+
+            //textWriter.WriteLine(result);
+
+            //textWriter.Flush();
+            //textWriter.Close();
+        }
+
+        private static string timeConversion(string? s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return "00:00:00";
+            }
+            
+            DateTime.TryParse(s, out DateTime time);
+            Console.WriteLine(time.TimeOfDay);
+            return time.TimeOfDay.ToString();
+        }
+    }
+}
